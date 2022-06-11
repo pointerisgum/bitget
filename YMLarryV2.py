c27acafebbe73987c1e19fb859339a90f479fa6b
pointerisgum <pointerisgum@gmail.com>
2022-06-11 23:01:38 +0900

    aaa

--- YMLarryV2.py
+++ YMLarryV2.py
@@ -947,7 +947,7 @@ def oneDay():
 
 oneDayJob = schedule.every(120).seconds.do(lambda: oneDay())
 schedule.cancel_job(oneDayJob)
-initTickers()
+# initTickers()
 schedule.every().day.at("01:01:01").do(lambda: initTickers())
 
 
